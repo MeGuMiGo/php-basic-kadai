932412dfe8c88cf6dc12fc840ac0081d643a0175
MeGuMiGo <131587237+MeGuMiGo@users.noreply.github.com>
2023-07-23 21:48:59 +0900

    Add files via upload

---
+++ kadai_021/confirmation.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>課題021</title>
+  </head>
+  <body>
+    <h2>入力内容をご確認ください</h2>
+    <P>お名前：<?php echo $_POST['user_name']; ?></P>
+    <p>性別：<?php echo $_POST['user_gender']; ?></p>
+    <p>メールアドレス：<?php echo $_POST['user_email']; ?></p>
+  </body>
+</html>
